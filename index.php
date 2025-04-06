0a715425acf8138d3867362fb7f615819653ee12
fonsekorz <fonsekaov84@proton.me>
2025-04-06 22:40:59 +0200

    cambios css shiny 06/04/25

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ function renderCards($pokeArray)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PokeWeb</title>
+    <title>PokeCartas</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
